f33e9b0373f714a36408eaa1c810940665198ee9
KarlStarkweather <stark13@usa.net>
2021-11-02 19:58:14 -0700

    debug submit btn

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
-  ("button#submitBtn").click(function() {
+  $("button#submitBtn").click(function() {
     const name = $("#name").val();
     const birthDate = $("#birthDate").val();
     const favFood = $("#favFood").val();
-    alert "food selected was " + favFood  
+    alert("food selected was " + favFood);  
   });
   
-  
-  
-
+ 
 });
 
 
