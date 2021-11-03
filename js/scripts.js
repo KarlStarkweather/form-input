37c7d326386b74a509dbeabb755e83a7e07548dc
KarlStarkweather <stark13@usa.net>
2021-11-02 20:41:26 -0700

    add branching functionality

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,12 @@ $(document).ready(function() {
     const favFood = $("#favFood").val();
     const gender = $("input:radio[name=gender]:checked").val();
     const housing = $("input:radio[name=housing]:checked").val();
-    alert("gender is " + gender + " and housing status is " + housing);  
+    if (gender === "nonBinary") {
+      if (favFood === "Pizza") {
+        alert("Sorry, Non Binary people can't have Pizza");
+      } 
+    }
+    // alert("gender is " + gender + " and housing status is " + housing);  
   });
   
  
