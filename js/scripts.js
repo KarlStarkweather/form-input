ece1c4ecda11f1463d7ae7fe2cabab2d655a79dc
KarlStarkweather <stark13@usa.net>
2021-11-02 19:49:28 -0700

    add submit button and js to store values so far

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  ("button#submitBtn").click(function() {
+    const name = $("#name").val();
+    const birthDate = $("#birthDate").val();
+    const favFood = $("#favFood").val();
+    alert "food selected was " + favFood  
+  });
+  
+  
+  
+
+});
+
+
